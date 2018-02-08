083ded43394c227f3a8a415c7985fdaf329b117b
Jogboms <jeremiahogbomo@gmail.com>
2018-02-08 17:51:37 +0100

    fix: lint issues

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,33 +1,35 @@
 export interface PickerOptions {
-    type?: string;
-    title?: string;
-    theme?: string;
-    maxDate?: Date;
-    minDate?: Date;
-    startingDate?: Date;
-    is24HourView: boolean;
-    maxTime?: {
-        hour: number;
-        minute: number;
-    };
-    minTime?: {
-        hour: number;
-        minute: number;
-    };
+  type?: string;
+  title?: string;
+  theme?: string;
+  maxDate?: Date;
+  minDate?: Date;
+  startingDate?: Date;
+  startingHour?: number;
+  startingMinute?: number;
+  is24HourView: boolean;
+  maxTime?: {
+    hour: number;
+    minute: number;
+  };
+  minTime?: {
+    hour: number;
+    minute: number;
+  };
 }
 export interface PickerResponse {
-    day?: number;
-    month?: number;
-    year?: number;
-    hour?: number;
-    minute?: number;
-} 
+  day?: number;
+  month?: number;
+  year?: number;
+  hour?: number;
+  minute?: number;
+}
 export declare class ModalDatetimepicker {
-    constructor();
-    pickDate(options?: PickerOptions): Promise<{}>;
-    pickTime(options?: PickerOptions): Promise<{}>;
-    private show(options);
-    chooseDate(): void;
-    chooseTime(): void;
-    close(response?: any): void;
+  constructor();
+  pickDate(options?: PickerOptions): Promise<{}>;
+  pickTime(options?: PickerOptions): Promise<{}>;
+  private show(options);
+  chooseDate(): void;
+  chooseTime(): void;
+  close(response?: any): void;
 }
